3ec81a80edfaba2e17669b73a412e586b67c0155
rkemmy <rkemmy.rk69@gmail.com>
2018-05-30 14:44:27 +0300

    change database

--- hoody/settings.py
+++ hoody/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'hoody.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hoody',
+        'USER': 'remmy',
+        'PASSWORD': 'bxt',
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=500)
