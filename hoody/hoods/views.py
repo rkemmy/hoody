4fd1c0e559904ca079eec6b301f8f6dbc943ab45
rkemmy <rkemmy.rk69@gmail.com>
2018-05-30 05:11:19 +0300

    create functions to leave, join and list hoods

--- hoody/hoods/views.py
+++ hoody/hoods/views.py
@@ -17,3 +17,38 @@ class SingleHood(generic.DetailView):
     model = Hood
 
 
+class ListHoods(generic.ListView):
+    model = Hood
+
+class JoinHood(LoginRequiredMixin, generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('hoods:single', kwargs={'slug': self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        hood = get_object_or_404(Hood, slug=self.kwargs.get('slug'))
+
+        try:
+            HoodMember.objects.create(user=self.request.user, hood=hood)
+        except IntegrityError:
+            messages.warning(self.request, 'You are already a member!')
+        else:
+            messages.success(self.request, 'Congratulatoins!! You are now a member!')
+
+        return super().get(request, *args, **kwargs)
+
+
+class LeaveHood(LoginRequiredMixin, generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('hoods:single', kwargs={'slug': self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+            membership = HoodMember.objects.filter(user=self.request.user, hood__slug=self.kwargs.get('slug')).get()
+        except HoodMember.DoesNotExist:
+            messages.warning(self.request, 'Sorry your are not a member here!')
+        else:
+            membership.delete()
+            messages.success(self.request, 'You are no longer a member!')
+
+        return super().get(request, *args, **kwargs)
