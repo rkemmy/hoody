d75f4dc775110bf799789d50c65ffd41c3bf89e8
rkemmy <rkemmy.rk69@gmail.com>
2018-05-30 05:09:27 +0300

    define a createhooh view function

--- hoody/hoods/views.py
+++ hoody/hoods/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse
+from django.views import generic
+from hoods.models import Hood, HoodMember
+from django.contrib import messages
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+# Create your views here
+
+class CreateHood(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'location')
+    model = Hood
+
+
