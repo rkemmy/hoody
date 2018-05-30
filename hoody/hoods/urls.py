f942ba4a07cd7284f50dc1fff0ff25f477380958
rkemmy <rkemmy.rk69@gmail.com>
2018-05-30 11:40:59 +0300

    fix error

--- hoody/hoods/urls.py
+++ hoody/hoods/urls.py
@@ -8,3 +8,4 @@ urlpatterns = [
     url(r'businesses/in/(?P<slug>[-\w]+)/$', views.SingleHood.as_view(), name='single'),
     url(r'join/(?P<slug>[-\w]+)/$', views.JoinHood.as_view(), name='join'),
     url(r'leave/(?P<slug>[-\w]+)/$', views.LeaveHood.as_view(), name='leave'),
+]
