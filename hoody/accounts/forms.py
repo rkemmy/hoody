088347218d8ddd80ab6d70150566bedf32c528c2
rkemmy <rkemmy.rk69@gmail.com>
2018-05-30 04:40:50 +0300

    correct form error

--- hoody/accounts/forms.py
+++ hoody/accounts/forms.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-class UserCreationForm(UserCreationForm):
+class UserCreateForm(UserCreationForm):
 
     class Meta:
-        fields = ('username', 'email', 'password', 'password1')
+        fields = ('username', 'email', 'password1')
         model = get_user_model()
 
     def __init__(self,*args,**kwargs):
