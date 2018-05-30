be09c84ffd7de83a78cbeaf6e8a3d2156904c74e
rkemmy <rkemmy.rk69@gmail.com>
2018-05-30 04:59:25 +0300

    Include loginrequired before rendering index page

--- hoody/hoody/views.py
+++ hoody/hoody/views.py
@@ -1,10 +1,6 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class HomePage(TemplateView):
+class HomePage(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
 
-class ThanksPage(TemplateView):
-    template_name = 'thanks.html'
-
-class TestPage(TemplateView):
-    template_name = 'test.html'
