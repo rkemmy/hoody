9360852db1748bc30fe465ee4438383b3b988e82
rkemmy <rkemmy.rk69@gmail.com>
2018-05-30 05:31:26 +0300

    register hood models

--- hoody/hoods/admin.py
+++ hoody/hoods/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class HoodMemberInline(admin.TabularInline):
+    model = models.HoodMember
+
+admin.site.register(models.Hood)
